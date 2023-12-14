1406efe73831c67d3f4723a0aad2e03f57c529d5
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-14 18:05:56 +0500

    Signalr

--- SignalRInBlazor/Program.cs
+++ SignalRInBlazor/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.AspNetCore.ResponseCompression;
 using SignalRInBlazor.Components;
+using SignalRInBlazor.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+          new[] { "application/octet-stream" });
+});
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
@@ -15,7 +22,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseResponseCompression();
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
@@ -23,5 +30,6 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+app.MapHub<ChatHub>("/chathub");
 
 app.Run();
